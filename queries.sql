2802c41b343f5d6de82929356c9fd7de75f84be0
AdamLumbley <adamwlumbley@gmail.com>
2026-02-19 02:02:20 -0700

    Add total revenue by product and polish query formatting

--- queries.sql
+++ queries.sql
@@ -9,7 +9,7 @@ JOIN Customers c ON s.customer_id = c.customer_id
 GROUP BY month, c.loyalty_tier
 ORDER BY month ASC, total_revenue DESC;
 
--- Total revenue per product category
+-- Total revenue by product category
 SELECT
     p.category,
     ROUND(SUM(o.quantity * o.price), 2) AS total_revenue
@@ -17,6 +17,16 @@ FROM Subscriptions s
 JOIN Orders o ON s.subscription_id = o.subscription_id
 JOIN Products p ON s.product_id = p.product_id
 GROUP BY p.category
+ORDER BY total_revenue DESC;
+
+-- Total revenue by product
+SELECT
+    p.name,
+    ROUND(SUM(o.quantity * o.price), 2) AS total_revenue
+FROM Subscriptions s
+JOIN Orders o ON s.subscription_id = o.subscription_id
+JOIN Products p ON s.product_id = p.product_id
+GROUP BY p.name
 ORDER BY total_revenue DESC;
 
 -- Promo code usage and total revenue per promo
